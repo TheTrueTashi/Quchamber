47804d6ca9379f515baa716be4d2c64588c421c5
TheTrueTashi <Luca.Cocco@gmx.de>
2019-03-03 21:31:42 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1551644965,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/shadowcaster-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/mask-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/projectile-sheet0.png",
+		"images/p_inpactshotgun.png",
+		"images/p_inpactenemy.png",
+		"images/shotgun-sheet0.png",
+		"images/s_mechanicshotgunsfx-sheet0.png",
+		"images/enemymask-sheet0.png",
+		"images/enemyspawn-sheet0.png",
+		"images/eenemyspawn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
